35bca72a887d48918fee481e8ffc96510598c8a8
fsossai <federico.sossai@gmail.com>
2021-05-23 14:30:43 +0200

    write_csrmat works

--- src/utils.f90
+++ src/utils.f90
@@ -1,5 +1,6 @@
 module utils
     use class_precision
+    use class_CSRMAT
     
 contains
 
@@ -13,5 +14,34 @@ contains
         write(unit=1, fmt='(i10,e15.8)') (i, x(i), i=1,n)
         close(unit=1)
     end subroutine
+
+    subroutine write_csrmat(name, A)
+        character(len=*), intent(in) :: name
+        type(CSRMAT), intent(in) :: A
+        integer, pointer :: iat(:), ja(:)
+        real(dp), pointer :: coef(:)
+        character(len=19) :: frmt = '(i7,1x,i7,1x,e15.8)'
+        integer :: n,i,j,rlen,c_start,c_end
+
+        ! creating handles
+        iat => A%patt%iat
+        ja => A%patt%ja
+        coef => A%coef
+        n = A%patt%nrows
+
+        open(unit=1, file=name)
+        write(1,*) n,A%patt%nterm
+        do i=1,n
+            c_start = iat(i)
+            c_end = iat(i+1) - 1
+            do j = c_start,c_end
+                write(1,*) i,ja(j),coef(j)
+            end do
+        end do
+        
+        close(unit=1)
+    end subroutine
+
+
 
 end module utils
