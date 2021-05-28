b79c5293a32a66b53db287cd9e187721a46e20dd
fsossai <federico.sossai@gmail.com>
2021-05-28 15:51:59 +0200

    Amdahl's Law fitting

--- benchmark/plot_speedup.py
+++ benchmark/plot_speedup.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import pandas as pd
+import scipy.optimize
+
+def amdahls_law(x, s):
+    return 1 / (s + (1 - s)/x)
 
 if len(sys.argv) < 2:
     print('ERROR: missing file name')
@@ -14,9 +18,16 @@ single = p * n
 ideal = range(1, max(data[0])+1)
 speedup = [single / x for x in data[1]]
 
+# calculating the fraction of serial program according
+# to Amdhal's Law
+s, _ = scipy.optimize.curve_fit(amdahls_law, data[0], speedup)
+s = s[0]
+print('s =', s)
+
 r = range(1, len(ideal)+1)
 plt.plot(data[0], speedup, '.-', label='Benchmarked')
 plt.plot(r, ideal, '--', label='Ideal')
+plt.plot(r, amdahls_law(np.array(r), s), '--', label=f"Amdahl's Law, s={s:.3}")
 plt.xticks(r, r)
 plt.title('BiCGSTAB - OpenMP strong scaling')
 plt.xlabel('Number of threads')
