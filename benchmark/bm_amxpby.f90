2f47f2139434fb7e6ccf55d2856b6333137164d2
fsossai <federico.sossai@gmail.com>
2021-05-28 16:46:34 +0200

    Time benchmark more flexible

--- benchmark/bm_amxpby.f90
+++ benchmark/bm_amxpby.f90
@@ -5,15 +5,17 @@ use omp_lib
 
 real(dp), parameter                 :: flops_per_term = 2
 real(dp), parameter                 :: flops_per_row = 2
-integer                             :: nn, nt, iterations, nthreads
+integer                             :: nn, nt, iterations
 type(CSRMAT)                        :: A
 real(dp), dimension(:), allocatable :: x, y, z
-real(dp)                            :: timer, total_flops
+real(dp)                            :: timer, total_flops, delta
+character(len=100)                  :: filename
 
-read(*,*) iterations
+read(*,'(a)') filename
+iterations = 10000
 
 ! reading matrix
-open(1, file='../inputs/mat1500.txt', status='old')
+open(1, file=filename, status='old')
 read(1, *) nn, nt
 ierr = new_CSRMAT(nn, nt, A)
 call readmat(1, .false., A, ierr)
@@ -29,19 +31,25 @@ print '(a25,i15)', 'Iterations:',       iterations
 print '(a25,i15)', 'Matrix size:',      nn
 print '(a25,i15)', 'Non-zero terms:',   nt
 
-timer = omp_get_wtime()
+timer = 0.0_dp
 !$omp parallel private(i)
-!$omp single
-nthreads = omp_get_num_threads()
-!$omp end single
 do i = 1, iterations
-    call amxpby(z, dble(i), A, x)
+    !$omp master
+    delta = omp_get_wtime()
+    !$omp end master
+    
+    call amxpby_set(z, dble(i), A, x)
+    
+    !$omp master
+    delta = omp_get_wtime() - delta
+    timer = timer + delta
+    !$omp end master
+    
+    !$omp barrier
 end do
 !$omp end parallel
-timer = omp_get_wtime() - timer
 
 total_flops = (flops_per_term * nt + flops_per_row * nn) * iterations
-print '(a25,i15)',    'Number of threads:', nthreads
 print '(a25,en15.3)', 'Elapsed time [s]:',  timer
 print '(a25,en15.3)', 'Avg time [s]:',      timer / iterations
 print '(a25,en15.3)', 'Performance [GFlops]:', total_flops / timer * 1e-9
