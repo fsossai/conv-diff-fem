6b2dd1aa3b369e43a088409d4a2538e584dda88b
fsossai <federico.sossai@gmail.com>
2021-06-11 21:49:13 +0200

    Basic assembly without global matrices update

--- src/fem.f90
+++ src/fem.f90
@@ -6,12 +6,19 @@ subroutine solve(coord, topo)
     real(dp), intent(in)        :: coord(:,:)
     integer, intent(in)         :: topo(:,:)
 
-    !type(CSRMAT)                :: H, B, P
+    type(CSRMAT)                :: H, B, P
     real(dp), allocatable       :: q(:)
     integer                     :: i, ne, nn
     integer                     :: nodes(3)
-    real(dp), dimension(3,3)    :: T, C
-    real(dp)                    :: area
+    real(dp), dimension(3,3)    :: T, C, He, Be, Pe
+    real(dp)                    :: area, grad(2,3), diff(2,2), qe, &
+                                   ones3x1(3,1), vel(1,2)
+
+    diff = 0.0_dp               ! Diffusivity coefficients
+    diff(1,1) = 1.0_dp
+    diff(2,2) = 1.0_dp
+    ones3x1 = 1.0_dp
+    vel = 1e-2_dp               ! (constant) velocity field
 
     ne = size(topo, 1)          ! Number of elements
     nn = size(coord, 1)         ! Number of nodes
@@ -27,8 +34,18 @@ subroutine solve(coord, topo)
         T(:, 2:3) = coord(nodes, :)
         area = abs(det3x3(T)) / 2.0_dp
         call inv3x3(T, C)
+        grad = C(2:3, :)
+        He = area * matmul(transpose(grad), matmul(diff, grad))
+        qe = area / 3.0_dp
+        Be = matmul(ones3x1, matmul(vel, grad) / 6.0_dp)
+
+        ! update global matrices
+        ! H(nodes, nodes) = H(nodes, nodes) + He
+        ! B(nodes, nodes) = B(nodes, nodes) + Be
+        ! q(nodes) = q(nodes) + qe
     end do
 
+    
     deallocate(q)
 
 end subroutine
