fd6fc21f641ee7fee55f0f7222c30171b19fea39
fsossai <federico.sossai@gmail.com>
2021-06-24 16:58:17 +0200

    Speedup plot with modified Amdahl's Law

--- benchmark/plot_speedup.py
+++ benchmark/plot_speedup.py
@@ -7,12 +7,20 @@ import scipy.optimize
 def amdahls_law(x, s):
     return 1 / (s + (1 - s)/x)
 
+def modified_amdahls_law(x, s, v):
+    return 1 / (s + (1 - s - v)/x + v * x)
+
 if len(sys.argv) < 2:
     print('ERROR: missing file name')
     sys.exit(-1)
 
 filename = sys.argv[1]
 data = pd.read_csv(filename, sep='\t', header=None)
+
+# Considering only the first 'sys.argv[2]' rows if specified
+if (len(sys.argv) == 3):
+    data = data[0:int(sys.argv[2])]
+    
 _, (n,p) = next(data.iterrows())
 single = p * n
 ideal = range(1, max(data[0])+1)
@@ -20,14 +28,18 @@ speedup = [single / x for x in data[1]]
 
 # calculating the fraction of serial program according
 # to Amdhal's Law
-s, _ = scipy.optimize.curve_fit(amdahls_law, data[0], speedup)
-s = s[0]
+par, _ = scipy.optimize.curve_fit(amdahls_law, data[0], speedup)
+#par, _ = scipy.optimize.curve_fit(modified_amdahls_law, data[0], speedup)
+s = par[0]
+#v = par[1]
 print('s =', s)
+#print('v =', v)
 
 r = range(1, len(ideal)+1)
 plt.plot(data[0], speedup, '.-', label='Benchmarked')
-plt.plot(r, ideal, '--', label='Ideal')
-plt.plot(r, amdahls_law(np.array(r), s), '--', label=f"Amdahl's Law, s={s:.3}")
+#plt.plot(r, ideal, '--', label='Ideal')
+plt.plot(r, amdahls_law(np.array(r), par[0]), '--', label=f"Amdahl's Law, s={s:.3}")
+#plt.plot(r, modified_amdahls_law(np.array(r), par[0]), '--', label=f"Modified Amdahl's Law, s={s:.3}")
 plt.xticks(r, r)
 plt.title('BiCGSTAB - OpenMP strong scaling')
 plt.xlabel('Number of threads')
